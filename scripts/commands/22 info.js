4b27d9d4377e955205c1253b66e5604f11181c00
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-09-01 11:48:21 +0600

    Update 22 info.js

--- scripts/commands/22 info.js
+++ scripts/commands/22 info.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "info",
   version: "1.0.1", 
   permssion: 0,
-  credits: "Ialamick Cyber Chat",
+  credits: "Ialamick Cyber Chat", //don't change the credits please
   prefix:true,
   description: "Admin and Bot info.",
   category: "...",
@@ -23,41 +23,36 @@ const time = process.uptime(),
     minutes = Math.floor((time % (60 * 60)) / 60),
     seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【hh:mm:ss】");
-var link = ["https://i.imgur.com/jfVTL4T.jpeg", 
-
-            "https://i.imgur.com/jfVTL4T.jpeg", 
-
-            "https://i.imgur.com/jfVTL4T.jpeg",
-
-            "https://i.imgur.com/jfVTL4T.jpeg"];
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+var link = ["https://i.imgur.com/qMe4oK0.jpg", "https://i.imgur.com/CpiigAY.jpg", "https://i.imgur.com/QzN6CGW.jpg", "https://i.imgur.com/ccfEyVR.jpg"];
 
 var callback = () => api.sendMessage({body:`•—»✨𝐀𝐝𝐦𝐢𝐧 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺
- •┄┅════❁🌺❁════┅┄•
+•┄┅═════❁🌺❁═════┅┄•
 
-𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : 𝐈𝐬𝐥𝐚𝐦𝐢𝐜𝐤 𝐂𝐲𝐛𝐞𝐫 𝐂𝐡𝐚𝐭
+ 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : 𝐒𝐡𝐚𝐨𝐧 𝐏𝐫𝐨𝐣𝐞𝐜𝐭
 
-𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 : 𝟕 𝐀d𝐦𝐢𝐧 𝐜𝐨𝐧𝐭𝐫𝐨𝐥 𝐓𝐡𝐢𝐬 𝐑𝐨𝐛𝐨𝐭
+𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 : শাওন আহমেদ
+
+𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : 𝐒𝐡𝐚𝐨𝐧 𝐀𝐡𝐦𝐞𝐝
 
 •┄┅══❁CONCATET❁══┅┄• 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐈𝐝: https://www.facebook.com/Hey.Its.Me.Shaon.Ahmed
 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐏𝐚𝐠𝐞 : https://www.facebook.com/profile.php?id=100081939442749
+𝐖𝐏 𝐍𝐮𝐦𝐛𝐞𝐫 : 01814587247
 
-𝐖𝐏  : wa.me/+8801859561262
-𝐖𝐏 : wa.me/+880 1309-991724
-𝐖𝐏 : wa.me/+880 1905-600093
-𝐖𝐏 : wa.me/+880 1771-717162
-𝐖𝐏 : wa.me/+880 1885-115218
-𝐖𝐏 : wa.me/+880 1763-899929
-𝐖𝐏 : wa.me/+880 1814-587247
+𝐒𝐞𝐚𝐫𝐜𝐡 𝐆𝐨𝐨𝐠𝐥𝐞 : শাওন আহমেদ
 
-•┄┅═══❁🌺❁═══┅┄•\n🌺✨𝐎𝐭𝐡𝐞𝐫𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺\n •┄┅═══❁🌺❁═══┅┄•
+𝐖𝐚𝐛 𝐒𝐢𝐭𝐞 𝐋𝐢𝐧𝐤 : https://linktr.ee/shaonahmed
 
-TYPE /help
+•┄┅═════❁🌺❁═════┅┄•\n🌺✨𝐎𝐭𝐡𝐞𝐫𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺\n •┄┅═════❁🌺❁═════┅┄•
+
+TYPE /admin 
 
 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : ${global.config.BOTNAME}
 
 𝐁𝐨𝐭 𝐏𝐫𝐞𝐟𝐢𝐱 : ${global.config.PREFIX}
+
+𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : 𝐒𝐡𝐚𝐨𝐧 𝐀𝐡𝐦𝐞𝐝
 
 •—»✨ 𝐔𝐩𝐭𝐢𝐦𝐞
 
@@ -65,6 +60,6 @@ TYPE /help
 
 𝐁𝐨𝐭 𝐈𝐬 𝐑𝐮𝐧𝐧𝐢𝐧𝐠 ${hours}:${minutes}:${seconds}.
 
-𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠  ༄🌺\n｢🕋｣${global.config.BOTNAME}｢🕋｣`,attachment: fs.createReadStream(__dirname + "/cache/cyber.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/cyber.jpg")); 
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/cyber.jpg")).on("close",() => callback());
+𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠  ༄🌺\n｢🕋｣${global.config.BOTNAME}｢🕋｣`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
    };
